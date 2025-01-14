6e2513ee979a40178e1f50b3f3b5c26a985be85c
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-14 14:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	if (s === "" || !s.includes(" ")) {
+		return s;
+	}
+	const spaceIndex = s.indexOf(" ");
+	return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
