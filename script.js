43f355ba8b955e37829ab8f890f9b54f11a32cb6
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-17 18:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function firstWord(s) {
   // your code here
-	if(s === ''){
-		return '';
+
+	if (s === "" || !s.includes(" ")) {
+		return s;
 	}
-	const spaceIndex = s.indexOf(' ');
-	if(spaceIndex === -1){
-		return str;
-	}
-	return s.slice(0, spaceIndex);
+	const spaceIndex = s.indexOf(" ");
+	return s.substring(0, spaceIndex);
 // Do not change the code below
 
 const s = prompt("Enter String:");
