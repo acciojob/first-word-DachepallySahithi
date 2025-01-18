3e3d0f0b2669600e71e86c39af0bb001329d82f4
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-18 14:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function firstWord(s) {
   // your code here
+	console.log("Input string:", s);
 	if(s==='' || !s.includes(' ')) {
+		console.log("Returning entire string:", s);
 		return s;
 	}
 	const spaceIndex = s.indexOf(' ');
-	return s.substring(0, spaceIndex);
+	console.log("Space index:", spaceIndex); 
+  const firstWord = s.substring(0, spaceIndex);
+  console.log("First word:", firstWord);
+  return firstWord;
 }
 // Do not change the code below
 
